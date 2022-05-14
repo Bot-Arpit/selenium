45ea5a4821afbe05b805c6f6b0a3ee1bab9cb4f6
Arpit <Arpit@MSI>
2022-05-14 20:56:39 +0530

    second

--- seleniumProject/src/test/java/seleniumProject/first.java
+++ seleniumProject/src/test/java/seleniumProject/first.java
@@ -8,7 +8,7 @@ public class first {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arpit\\eclipse-workspace\\seleniumProject\\src\\main\\resources\\Webdriver\\Chromedriver\\chromedriver.exe");
 		 WebDriver  driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
+		driver.get("");
 	}
 
 }
